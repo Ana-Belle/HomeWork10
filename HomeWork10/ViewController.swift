797cc4ca4476ec799522c2c10a966dc8b880fa8f
Anastasia Belyakova <ana-belle@mail.ru>
2021-09-10 21:55:16 +0300

    All comments have been deleted.

--- HomeWork10/ViewController.swift
+++ HomeWork10/ViewController.swift
@@ -1,17 +1,9 @@
-//
-//  ViewController.swift
-//  HomeWork10
-//
-//  Created by Anastasia Belyakova on 10.09.2021.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
 
     func setupView() {
